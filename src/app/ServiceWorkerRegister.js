7562c7d08f8333673b53d9a2a9487dd98fb6be7a
ahmad655656 <ahmadsalloum8899@gmail.com>
2025-10-15 20:25:23 +0300

    updates

--- src/app/ServiceWorkerRegister.js
+++ src/app/ServiceWorkerRegister.js
@@ -3,20 +3,19 @@
 import { useEffect } from "react";
 
 export default function ServiceWorkerRegister() {
-  useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      window.addEventListener("load", () => {
-        navigator.serviceWorker
-          .register("/sw.js")
-          .then((registration) => {
-            console.log("✅ Service Worker مسجل بنجاح:", registration);
-          })
-          .catch((error) => {
-            console.log("❌ فشل تسجيل Service Worker:", error);
-          });
+ useEffect(() => {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((registration) => {
+        console.log("✅ Service Worker مسجل بنجاح:", registration);
+      })
+      .catch((error) => {
+        console.error("❌ فشل تسجيل Service Worker:", error);
       });
-    }
-  }, []);
+  }
+}, []);
+
 
   return null;
 }
